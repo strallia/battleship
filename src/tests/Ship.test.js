5f7b96a2b107e73d89f5ff8ab050368831a35311
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-03-04 11:12:10 -0800

    test: Replace test keyword with it alias

--- src/tests/Ship.test.js
+++ src/tests/Ship.test.js
@@ -1,30 +1,30 @@
 import Ship from '../classes/Ship';
 
-test('Hitting ship increments its hit score by one per hit', () => {
+it('Hitting ship increments its hit score by one per hit', () => {
   const ship = new Ship();
   ship.hit();
   ship.hit();
   expect(ship.hits).toBe(2);
 });
 
-test('Un-sunken ship returns accurate sunk status', () => {
+it('Un-sunken ship returns accurate sunk status', () => {
   const ship = new Ship();
   expect(ship.isSunk()).toBe(false);
 });
 
-test('Sunken ship returns accurate sunk status', () => {
+it('Sunken ship returns accurate sunk status', () => {
   const destoryer = new Ship('destroyer');
   destoryer.hit();
   destoryer.hit();
   expect(destoryer.isSunk()).toBe(true);
 });
 
-test('Cruiser has length of 3', () => {
+it('Cruiser has length of 3', () => {
   const cruiser = new Ship('cruiser');
   expect(cruiser.length).toBe(3);
 });
 
-test('Carrier has length of 5', () => {
+it('Carrier has length of 5', () => {
   const carrier = new Ship('carrier');
   expect(carrier.length).toBe(5);
 });

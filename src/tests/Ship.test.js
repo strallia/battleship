a8a3f0130a57bb8f91fa9583abe6309cb92649cb
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-03-03 23:14:15 -0800

    test: Write tests for ship length

--- src/tests/Ship.test.js
+++ src/tests/Ship.test.js
@@ -11,3 +11,13 @@ test('Ship returns accurate sunk status', () => {
   const ship = new Ship();
   expect(ship.isSunk).toBe(false);
 });
+
+test('Cruiser has length of 3', () => {
+  const cruiser = new Ship('cruiser');
+  expect(cruiser.length).toBe(5);
+});
+
+test('Carrier has length of 5', () => {
+  const carrier = new Ship('carrier');
+  expect(carrier.length).toBe(5);
+});

3a5277715de4b7ad7124a8eb2e2a69f19f769e92
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-03-05 22:06:53 -0800

    feat(gameboard): Disallow ship placement past right side of board

--- src/classes/Gameboard.js
+++ src/classes/Gameboard.js
@@ -1,22 +1,27 @@
 export default class Gameboard {
-  // Creates 10x10 grid with origin being the top left.
-  // Position selected by [y, x] coordinates where
+  // creates 10x10 grid with origin being the top left.
+  // position selected by [y, x] coordinates where
   // y = row number and x = column number.
   static board = Array(10)
     .fill(null)
     .map(() => Array(10).fill(null));
 
-  static placeShip(clickedCoord, shipInstance, direction) {
+  static placeShip(clickedCoord, ship, direction) {
     const y = clickedCoord[0];
     const x = clickedCoord[1];
 
+    // check if ship is off right side of board
+    const sternCoord = x + ship.length;
+    if (sternCoord > 9) return false;
+
+    // place ship on board
     let yCount = 0;
     let xCount = 0;
     while (
-      (yCount === 0 && xCount < shipInstance.length) ||
-      (yCount < shipInstance.length && xCount === 0)
+      (yCount === 0 && xCount < ship.length) ||
+      (yCount < ship.length && xCount === 0)
     ) {
-      Gameboard.board[y + yCount][x + xCount] = shipInstance;
+      Gameboard.board[y + yCount][x + xCount] = ship;
       if (direction === 'horizontal') xCount += 1;
       else yCount += 1;
     }
--- src/tests/Gameboard.test.js
+++ src/tests/Gameboard.test.js
@@ -1,8 +1,9 @@
 import Ship from '../classes/Ship';
 import Gameboard from '../classes/Gameboard';
 
+const battleship = new Ship(5);
+
 it('Places battleship on board vertically', () => {
-  const battleship = new Ship(5);
   const clickedCoord = [1, 2]; // [y(row), x(column)]
   Gameboard.placeShip(clickedCoord, battleship);
   expect(Gameboard.board[1][2]);
@@ -13,7 +14,6 @@ it('Places battleship on board vertically', () => {
 });
 
 it('Places battleship on board horizontally', () => {
-  const battleship = new Ship(5);
   const clickedCoord = [1, 2];
   Gameboard.placeShip(clickedCoord, battleship, 'horizontal');
   expect(Gameboard.board[1][2]).toBeInstanceOf(Ship);
@@ -22,3 +22,19 @@ it('Places battleship on board horizontally', () => {
   expect(Gameboard.board[1][5]).toBeInstanceOf(Ship);
   expect(Gameboard.board[1][6]).toBeInstanceOf(Ship);
 });
+
+it('Does not allow placement of horizontal ship past right side of board', () => {
+  const clickedCoord = [9, 9];
+  expect(Gameboard.placeShip(clickedCoord, battleship, 'horizontal')).toBe(
+    false,
+  );
+});
+
+// it('Does not allow placement of horizontal ship past left side of board', () => {
+//   const clickedCoord = [9, 9];
+//   expect(Gameboard.placeShip(clickedCoord, battleship, 'horizontal')).toBe(
+//     false,
+//   );
+// });
+// it('Does not allow placement of vertical ship past top side of board', () => {});
+// it('Does not allow placement of vertical ship past bottom side of board', () => {});

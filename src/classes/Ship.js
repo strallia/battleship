6ddffed5d4266dac5a1507329658809e61e540a9
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-03-06 19:26:07 -0800

    refactor(ship): Make ship properties private

--- src/classes/Ship.js
+++ src/classes/Ship.js
@@ -1,15 +1,27 @@
 export default class Ship {
+  #length;
+
+  #hits;
+
   constructor(length) {
-    this.length = length;
-    this.hits = 0;
+    this.#length = length;
+    this.#hits = 0;
+  }
+
+  getLength() {
+    return this.#length;
+  }
+
+  getHits() {
+    return this.#hits;
   }
 
   hit() {
-    this.hits += 1;
+    this.#hits += 1;
   }
 
   isSunk() {
-    if (this.length === this.hits) return true;
+    if (this.#length === this.#hits) return true;
     return false;
   }
 }
--- src/tests/Ship.test.js
+++ src/tests/Ship.test.js
@@ -4,12 +4,12 @@ it('Increments hit score by one each time when ship hit', () => {
   const ship = new Ship();
   ship.hit();
   ship.hit();
-  expect(ship.hits).toBe(2);
+  expect(ship.getHits()).toBe(2);
 });
 
 it('Builds ship of length 5', () => {
   const ship = new Ship(5);
-  expect(ship.length).toBe(5);
+  expect(ship.getLength()).toBe(5);
 });
 
 it('Returns false when ship has not sunk', () => {

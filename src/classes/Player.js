4e28aeadf059f44e2702b69179c8efb85ff9b158
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-03-08 13:33:45 -0800

    feat(player): Creates board for each player

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -1,5 +1,12 @@
+import Gameboard from './Gameboard';
+
 export default class Player {
   constructor(name) {
     this.name = name;
+    this.gameboard = new Gameboard();
+  }
+
+  getBoard() {
+    return this.gameboard;
   }
 }
--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -1,6 +1,18 @@
 import Player from '../classes/Player';
+import Gameboard from '../classes/Gameboard';
 
 it("Returns player's name", () => {
   const player1 = new Player('Leah');
   expect(player1.name).toBe('Leah');
+});
+
+it("Returns player's own board", () => {
+  const player1 = new Player('Leah');
+  expect(player1.getBoard()).toBeInstanceOf(Gameboard);
+});
+
+it("Each players's board is a separate gameboard instance", () => {
+  const player1 = new Player('Leah');
+  const player2 = new Player('Bob');
+  expect(player1.getBoard()).not.toBe(player2.getBoard());
 });

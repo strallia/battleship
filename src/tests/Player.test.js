245496cd165602ce14d3260b8df11353f05f6fa4
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-03-08 13:04:22 -0800

    refactor(player): Remove getter for name property

--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -1,6 +1,6 @@
 import Player from '../classes/Player';
 
-it('Returns players name', () => {
+it("Returns player's name", () => {
   const player1 = new Player('Leah');
-  expect(player1.getName()).toBe('Leah');
+  expect(player1.name).toBe('Leah');
 });

e9b3ce565569178e73699654e1b50a50109261d0
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-03-09 22:13:17 -0800

    feat(DOM): Play round and update boards when send attack

--- src/modules/ScreenController.js
+++ src/modules/ScreenController.js
@@ -1,29 +1,43 @@
-import { getBoard } from './GameController';
+import { getBoard, playRound } from './GameController';
 
 const shipsBoardDiv = document.querySelector('.ships.board');
 const attacksBoardDiv = document.querySelector('.attacks.board');
 
 const updateScreen = () => {
+  // clear out all boards
+  shipsBoardDiv.textContent = '';
+  attacksBoardDiv.textContent = '';
+
   // reload active player's boards
   const activePlayerBoard = getBoard();
-
   activePlayerBoard.forEach((row, rowIndex) => {
     row.forEach((column, columnIndex) => {
-      const shipBoardButton = document.createElement('button');
-      const attackBoardButton = document.createElement('button');
+      const shipsBoardButton = document.createElement('button');
+      const attacksBoardButton = document.createElement('button');
 
       // color ships
       const square = activePlayerBoard[rowIndex][columnIndex];
       const hasShip = square === null ? false : Object.hasOwn(square, 'ship');
       if (hasShip) {
-        shipBoardButton.classList.add('ship');
+        shipsBoardButton.classList.add('ship');
       }
 
-      shipsBoardDiv.appendChild(shipBoardButton);
-      attacksBoardDiv.appendChild(attackBoardButton);
+      // add data attributes for coordinates
+      shipsBoardButton.dataset.y = rowIndex;
+      shipsBoardButton.dataset.x = columnIndex;
+      attacksBoardButton.dataset.y = rowIndex;
+      attacksBoardButton.dataset.x = columnIndex;
+
+      shipsBoardDiv.appendChild(shipsBoardButton);
+      attacksBoardDiv.appendChild(attacksBoardButton);
     });
   });
 };
 updateScreen();
 
-const handleBoardClick = () => {};
+const handleBoardClick = (targetSquare) => {
+  const { y, x } = targetSquare.dataset;
+  playRound([y, x]);
+  updateScreen();
+};
+attacksBoardDiv.addEventListener('click', (e) => handleBoardClick(e.target));

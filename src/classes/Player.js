2d804acf34546da63fee77475c3b6116271f8790
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-03-10 11:37:17 -0700

    refactor(player): Remove redundant getBoard method

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -2,9 +2,4 @@ export default class Player {
   constructor(name) {
     this.name = name;
   }
-
-  // TODO: remove this method and its tests (already in GameController)
-  getBoard() {
-    return this.gameboard.board;
-  }
 }
--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -4,14 +4,3 @@ it("Returns player's name", () => {
   const player1 = new Player('Leah');
   expect(player1.name).toBe('Leah');
 });
-
-it("Returns player's own board", () => {
-  const player1 = new Player('Leah');
-  expect(player1.getBoard()).toBeInstanceOf(Array);
-});
-
-it("Each players's board is a separate gameboard instance", () => {
-  const player1 = new Player('Leah');
-  const player2 = new Player('Bob');
-  expect(player1.getBoard()).not.toBe(player2.getBoard());
-});

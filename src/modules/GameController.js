9cc62371593aabd9572d647fbe72d4956e7e43ca
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-03-09 21:49:35 -0800

    feat(DOM): Display ships on ships-board

--- src/modules/GameController.js
+++ src/modules/GameController.js
@@ -1,5 +1,6 @@
 import Gameboard from '../classes/Gameboard';
 import Player from '../classes/Player';
+import Ship from '../classes/Ship';
 
 const player = [
   {
@@ -11,6 +12,24 @@ const player = [
     board: new Gameboard(),
   },
 ];
+
+// Only for building initial UI. Places ships on both player's boards.
+const placeShips = () => {
+  player[0].board.placeShip([1, 2], new Ship(5), false);
+  player[0].board.placeShip([6, 9], new Ship(4), false);
+  player[0].board.placeShip([5, 3], new Ship(3), false);
+  player[0].board.placeShip([0, 9], new Ship(3), false);
+  player[0].board.placeShip([1, 7], new Ship(2), false);
+
+  player[1].board.placeShip([1, 2], new Ship(5), false);
+  player[1].board.placeShip([3, 9], new Ship(4), false);
+  player[1].board.placeShip([6, 7], new Ship(3), false);
+  player[1].board.placeShip([2, 5], new Ship(3), false);
+  player[1].board.placeShip([8, 3], new Ship(2), false);
+};
+console.log(player[0].board.board);
+console.log(player[1].board.board);
+placeShips();
 
 let activePlayer = player[0];
 
--- src/modules/ScreenController.js
+++ src/modules/ScreenController.js
@@ -1,16 +1,24 @@
 import { getBoard } from './GameController';
 
-const shipsBoardDiv = document.querySelector('.ships-board');
-const attacksBoardDiv = document.querySelector('.attacks-board');
+const shipsBoardDiv = document.querySelector('.ships.board');
+const attacksBoardDiv = document.querySelector('.attacks.board');
 
 const updateScreen = () => {
   // reload active player's boards
   const activePlayerBoard = getBoard();
 
-  activePlayerBoard.forEach((row) => {
-    row.forEach((square) => {
+  activePlayerBoard.forEach((row, rowIndex) => {
+    row.forEach((column, columnIndex) => {
       const shipBoardButton = document.createElement('button');
       const attackBoardButton = document.createElement('button');
+
+      // color ships
+      const square = activePlayerBoard[rowIndex][columnIndex];
+      const hasShip = square === null ? false : Object.hasOwn(square, 'ship');
+      if (hasShip) {
+        shipBoardButton.classList.add('ship');
+      }
+
       shipsBoardDiv.appendChild(shipBoardButton);
       attacksBoardDiv.appendChild(attackBoardButton);
     });
